c5d7b17d0df88d83d5008eef63b9aac8aca2841d
asemElkhouli <asemelkhouli209@gmail.com>
2023-07-30 23:09:56 +0300

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Asem on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
